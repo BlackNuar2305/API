9b2bf0e21c7785d8e407f96d628694ec1c8954d4
black_nuar <hasaevsuliman6@gmail.com>
2023-07-16 12:45:20 +0300

    Seminar 6

---
+++ main.java
@@ -0,0 +1,13 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Deque deque = new ArrayDeque<>();
+
+    }
+}
---
+++ seminar6/hw1.java
@@ -0,0 +1,61 @@
+package seminar6;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class hw1 {
+    public static void main(String[] args) {
+        Map<String, ArrayList> map = new HashMap<>();
+        while (true) {
+            AddingData(map);
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Добавить новое имя? 1 - да, 0 - нет");
+            String string = scanner.nextLine();
+            if (string.equals("1")) continue;
+            else break;
+        }
+        sort(map);
+    }
+    public static Map<String, ArrayList> AddingData (Map<String, ArrayList> map) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        System.out.println("Введите Имя: ");
+        String str = scanner.nextLine();
+        while (true) {
+            System.out.println("Введите номер: ");
+            int number = Integer.parseInt(scanner.nextLine());
+            arrayList.add(number);
+            map.put(str, arrayList);
+            System.out.println("Добавить еще один номер? 1 - да, 0 - нет");
+            String str1 = scanner.nextLine();
+            if (str1.equals("1")) continue;
+            else if (str1.equals("0")) break;
+            else {
+                System.out.println("Введена неправильная команда");
+                break;
+            }
+        }
+        return map;
+    }
+
+    public static void sort(Map<String, ArrayList> map) {
+        int min = 10000000;
+        int max = 0;
+        for (var item : map.entrySet()) {
+            if (max < item.getValue().size())
+                max = item.getValue().size();
+            if (min > item.getValue().size())
+                min = item.getValue().size();
+        }
+        int n = max;
+        while (n >= min) {
+            for (var item : map.entrySet()) {
+                if (n == item.getValue().size())
+                    System.out.println(item.getKey() + " " + item.getValue());
+            }
+            n--;
+        }
+    }
+}
